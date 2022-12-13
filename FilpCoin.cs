f39e2f05070055416fe50a1b89ddabb5da4c82a7
Santoshi0731 <santoshipallaye27@gmail.com>
2022-12-13 08:03:45 +0530

    The Program for flipcoin

---
+++ FilpCoin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class FlipCoin
+    {
+        public static void FlipTheCoin()
+        {
+            int tailcount = 0;
+            int headcount = 0;
+
+
+            Console.WriteLine("Enter the number of flips");
+            int flip =Convert.ToInt32(Console.ReadLine());
+
+            if (flip > 0)
+            {
+                for (int i = 0; i < flip; i++)
+                {
+                    Random num = new Random();
+                    //Console.WriteLine(num.Next(0,2));
+                    int result = num.Next(0, 2);
+
+                    if (result == 0)
+                    {
+                        tailcount++;
+                    }
+                    else
+                    {
+                        headcount++;
+                    }
+
+                }
+                Console.WriteLine("HeadCount is " + headcount);
+                Console.WriteLine("TailCount is " + tailcount);
+
+              
+            }
+            else
+            {
+                Console.WriteLine("Please Enter the correct value");
+            }
+            
+        
+        }
+
+    }
+}
