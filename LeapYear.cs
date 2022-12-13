d553bcbba55451e0e97b3e6bea8c4b7af57cd3f0
Santoshi0731 <118991307+Santoshi0731@users.noreply.github.com>
2022-12-13 08:29:01 +0530

    Add files via upload

---
+++ LeapYear.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class LeapYear
+    {
+        public static void LeapYear1()
+        {
+           
+            Console.WriteLine("Enter the Year");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+
+            if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0))
+            {
+                Console.WriteLine("Its Leap Year");
+            }
+            else
+            {
+                Console.WriteLine("Its Not Leap Year");
+                
+            }
+        }
+    }
+}
+    
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HelloWorld
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //int val = 7;
+            //double val1 = 2.3;
+            //char val2 = 'r';
+            //bool val3 = true;
+            //string val4 = "H";
+            //Console.WriteLine("Integer Value" + val);
+            //Console.WriteLine("Double Value" + val1);
+            //Console.WriteLine("char Value" + val2);
+            //Console.WriteLine("boolean Value" + val3);
+            //Console.WriteLine("String value" + val4);
+
+            //Console.WriteLine("Enter the username");
+            //string username = Console.ReadLine();
+            //Console.WriteLine("The value given by user " + username);
+
+
+            //Console.WriteLine("Please provide 1st to add");
+            
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Please provide 1st to add");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int num3 = num1+ num2;
+            //Console.WriteLine("The Addition of 2 number"+num3);
+
+            //Console.WriteLine("Please enter the username");
+            //string username = Console.ReadLine();
+            //int num = username.Length;
+
+            //Console.WriteLine(num < 3);
+
+            
+            FlipCoin.FlipTheCoin();
+
+            
+            LeapYear.LeapYear1();
+
+            Fact.Fact1();
+                
+        }
+        
+    }
+}
